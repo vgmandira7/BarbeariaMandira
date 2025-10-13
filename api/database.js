f705b3a7b255711f16fd4efe3fe52799b10a06ae
vgmandira7 <vg.mandira@gmail.com>
2025-10-13 00:46:40 -0300

    Commit final

--- api/database.js
+++ api/database.js
@@ -1,12 +1,11 @@
 // api/database.js: Conversão para CJS (CommonJS)
 const { createClient } = require("@libsql/client");
-require('dotenv').config(); // Correção: require('dotenv').config() para CJS
 
 // Conexão com o Turso
 const db = createClient({
-  // As variáveis de ambiente serão lidas do Vercel
-  url: process.env.TURSO_DATABASE_URL,
-  authToken: process.env.TURSO_AUTH_TOKEN
+  // As variáveis de ambiente serão lidas do Vercel
+  url: process.env.TURSO_DATABASE_URL,
+  authToken: process.env.TURSO_AUTH_TOKEN
 });
 
 module.exports = db;
