79a41725d37b31acf7afd3a9c7fbb626d4fc8f81
Vitor Mandira <vg.mandira@gmail.com>
2025-11-16 23:46:04 -0300

    feat: remover tela de login do barbeiro

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -1,17 +1,9 @@
-import { useState } from "react";
-import AdminLogin from "@/components/AdminLogin";
 import AdminPanel from "@/components/AdminPanel";
 
 const AdminPage = () => {
-  const [logado, setLogado] = useState(false);
-
-  return logado ? (
-    <AdminPanel />
-  ) : (
-    <div className="flex items-center justify-center min-h-screen bg-background">
-      <div className="w-full max-w-md p-6">
-        <AdminLogin onAdminLogin={() => setLogado(true)} />
-      </div>
+  return (
+    <div className="min-h-screen bg-background">
+      <AdminPanel />
     </div>
   );
 };
